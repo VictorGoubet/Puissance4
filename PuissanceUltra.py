9606e564d277a58e9ca17aeb2abe01f520eb8154
Victor Goubet <victor.goubet@edu.devinci.fr>
2021-05-16 14:55:31 +0200

    refactorize again

--- PuissanceUltra.py
+++ PuissanceUltra.py
@@ -1,67 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr  3 08:21:38 2020
-
-@author: victo
-"""
 import time
 import numpy as np
 
 
 
-profondeure = -1
-profondeureMax = 3
-
-CRED = '\33[41m'
-CBLUE = '\33[44m'
-CYEL = '\33[36m'
-CEND = '\033[0m'
-
-def diag(s, k):
-    d1, d2 = [], []
-    i, j = (0, k) if k>0 else (-k, 0)
-    while i and j >0:
-        i, j = i-1, j-1 
-    while i < len(s) and j < len(s[0]):
-        d1.append(s[i, j])
-        d2.append(s[i, len(s[0])-j-1])
-        i, j = i+1, j+1
-    return d1, d2
-
-
-
-def TerminalUtility(s):
-    for i in s:
-        for n in range(9):
-            if i[n] == i[1+n] == i[2+n] == i[3+n] != 0:
-                return [True, 99999] if(i[n]==1) else [True, -99999]
-                  
-    for j in range(12):   
-        x=s[:, j]
-        for n in range(3):
-            if x[n] == x[1+n] == x[2+n] == x[3+n] != 0:
-                return [True,99999] if(x[n]==1) else [True,-99999]
-    
-    for k in range(-2, 9):
-        d1, d2 = diag(s, k)
-        for n in range(len(d1)-3): 
-            if d1[n] == d1[1+n] == d1[2+n] == d1[3+n] != 0:
-                return [True, 99999] if(d1[n]==1) else [True, -99999]
-            
-            if d2[n] == d2[1+n] == d2[2+n] == d2[3+n] != 0:
-                return [True, 99999] if(d2[n]==1) else [True, -99999]
-
-    nbP = np.sum(s==0)
-    if(nbP==30):
-        return [True, 50]
-   
-    return [False]
-            
-    
-
-
 def heuristique(s):
-    
+
     def checkLCD(tab):
         resLocal, resAdversaire = [True, 0], [True, 0]
         
@@ -69,12 +12,9 @@ def heuristique(s):
             if k == 2:
                 resLocal[0] = False
                 resAdversaire[1] += 1
-
             elif k == 1:                
                 resAdversaire[0] = False
                 resLocal[1] += 1
-
- 
         return resLocal, resAdversaire
 
     def EvalLCD(LCD, index):
@@ -83,7 +23,7 @@ def heuristique(s):
         tab = np.concatenate([tab2, tab1])
 
         fitnessLocal, fitnessAdversaire = 0, 0
-         
+            
         for k in range(len(tab)-3):
             check = checkLCD(tab[k:4+k])
             if check[0][0]:
@@ -92,7 +32,6 @@ def heuristique(s):
             if check[1][0]:
                 fitnessAdversaire += 3**check[1][1]       
         return fitnessLocal,fitnessAdversaire
-    
 
     def EvalAction(a):
             
@@ -117,8 +56,8 @@ def heuristique(s):
         
         while i < len(s) and j < len(s[0]):
             d1.append(s[i,j])
-            i,j=i+1,j+1
-              
+            i, j = i+1, j+1
+                
         d2 = []
         i, j = a
         while i < len(s) and j >= 0:
@@ -142,9 +81,9 @@ def heuristique(s):
             info = EvalLCD(d2, i2)
             fitnessLocal += info[0]
             fitnessAdversaire += info[1]
- 
-        return fitnessLocal, fitnessAdversaire
 
+        return fitnessLocal, fitnessAdversaire
+    
     score = 0
     for a in action(s):
         fitnessS = EvalAction(a)
@@ -155,27 +94,44 @@ def heuristique(s):
         
 
 
-def action(s):
-
-    actions = []
-    for k in range(12):
-        tab = s[:, k]
-        if tab[-1] == 0:
-            actions.append([len(tab)-1, k])
-        elif tab[0] == 0:
-            i = -1
-            while i-1 >= -6 and tab[i-1] != 0:
-                i-=1
-            actions.append([len(tab)+i-1, k])
-    return actions 
-       
+def diag(s, k):
+    d1, d2 = [], []
+    i, j = (0, k) if k > 0 else (-k, 0)
+    while i and j >0:
+        i, j = i-1, j-1 
+    while i < len(s) and j < len(s[0]):
+        d1.append(s[i, j])
+        d2.append(s[i, len(s[0])-j-1])
+        i, j = i+1, j+1
+    return d1, d2
 
 
-def Result(s, a, j):
-    res = np.copy(s)
-    res[a[0], a[1]] = j
-    return res
 
+def TerminalUtility(s):
+    for i in s:
+        for n in range(9):
+            if i[n] == i[1+n] == i[2+n] == i[3+n] != 0:
+                return [True, 99999] if i[n] == 1 else [True, -99999]
+                  
+    for j in range(12):   
+        x = s[:, j]
+        for n in range(3):
+            if x[n] == x[1+n] == x[2+n] == x[3+n] != 0:
+                return [True, 99999] if x[n] == 1 else [True, -99999]
+    
+    for k in range(-2, 9):
+        d1, d2 = diag(s, k)
+        for n in range(len(d1)-3): 
+
+            if d1[n] == d1[1+n] == d1[2+n] == d1[3+n] != 0:
+                return [True, 99999] if d1[n] == 1 else [True, -99999]
+            if d2[n] == d2[1+n] == d2[2+n] == d2[3+n] != 0:
+                return [True, 99999] if d2[n] == 1 else [True, -99999]
+
+    if np.sum(s==0) == 30:
+        return [True, 50]
+   
+    return [False]
 
 
 
@@ -202,6 +158,7 @@ def Max_Value(s, A, B):
         return v
 
 
+
 def Min_Value(s, A, B):
 
     global profondeure
@@ -222,6 +179,26 @@ def Min_Value(s, A, B):
             B = min(B, v[0])
         return v
             
+
+
+def Result(s, a, j):
+    res = np.copy(s)
+    res[a[0], a[1]] = j
+    return res
+
+
+
+def action(s):
+    actions = []
+    for k in range(len(s[0])):
+        i, tab = 1, s[:, k]
+        n = len(tab)
+        while i <= n and tab[-i] != 0:
+            i += 1   
+        if i <= n:
+            actions.append([n-i, k])
+    return actions 
+
 
 
 def MinMax(s):
@@ -245,24 +222,23 @@ def MinMax(s):
 
 def affichage(plateau):
 
-    print("\n| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |11 |12 | ")
-    print("\n------------------------------------------------- ")
+    print("\n| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |10 |11 |12 | \n")
+    print("-"*49)
     for i in plateau:
         print('| ', end='')
         for j in i:
-            if j==0:
+            if j == 0:
                 print('. | ', end='')           
-            elif j==1:
-                print(CRED+'X'+CEND+' | ', end='') 
+            elif j == 1:
+                print(f"{CRED}X{CEND} | ", end='') 
             else:
-                print(CBLUE+'O'+CEND+' | ', end='')
+                print(f"{CBLUE}O{CEND} | ", end='')
         print('\n')
-    print("\n")
+    print('\n')
 
 
 
 def SaisieSecur(s):
-
     c = input("colonne n° : ")
     while (c.isdigit() and 0 <= int(c)-1 < 12 and 0 in s[:, int(c)-1]) == False:
         c = input("colonne n° : ")
@@ -281,40 +257,36 @@ def SaisieSecur(s):
 def MorpionGame():
 
     j = input("Qui commence ?\n 1: Notre IA \n 2: L'IA adverse \n\n →  ")
-    j = 1 if j != "1" and j != "2" else int(j)
+    j = 1 if j not in ["1", "2"] else int(j)
 
-    grid = np.zeros((6, 12))
-    listeT = []
+    names = ["Notre IA", "L'adversaire"]
+    grid, listeT = np.zeros((6, 12)), []
+
     affichage(grid)
-        
     term = TerminalUtility(grid)
+    
     nbCoup = 0
     while term[0] == False:
-        
-        if j==2:
+
+        if j == 2:
             print("C'est le tour de l'adversaire, rentrez la colonne qu'elle a joué : ")
-            t1 = time.time()
-            saisie = SaisieSecur(grid)
-            dt = time.time()-t1
-            print(f"L'adversaire a réflechis {CYEL}{round(dt, 1)}{CEND} sc et joue en {CYEL}{saisie[1]+1}{CEND}")
-                
-            grid[saisie[0], saisie[1]] = 2
-            affichage(grid)
-            j = 1
-         
+            t = time.time()
+            action = SaisieSecur(grid)
+            dt = time.time() - t
         else:
             print("Le programme reflechi... :")
-            t1 = time.time()
+            t = time.time()
             action = MinMax(grid)
-            dt = time.time() - t1
+            dt = time.time() - t
                 
             listeT.append(dt)
             nbCoup += 1
-            print(f"Notre IA a réflechie {CYEL}{round(dt, 1)}{CEND} sc et joue en {CYEL}{action[1]+1}{CEND}")
-            grid[action[0], action[1]] = 1
-            affichage(grid) 
-            j = 2
-        
+            
+        print(f"{names[j-1]} a réflechis {CYEL}{round(dt, 1)}{CEND} sc et joue en {CYEL}{action[1]+1}{CEND}")
+        grid[action[0], action[1]] = j
+        j = 1 if j == 2 else 2
+
+        affichage(grid)
         term = TerminalUtility(grid)
         
         
@@ -333,4 +305,13 @@ def MorpionGame():
             
         
 if __name__ == "__main__":
+
+    profondeure = -1
+    profondeureMax = 3
+
+    CRED = '\33[41m'
+    CBLUE = '\33[44m'
+    CYEL = '\33[36m'
+    CEND = '\033[0m'
+
     MorpionGame()
